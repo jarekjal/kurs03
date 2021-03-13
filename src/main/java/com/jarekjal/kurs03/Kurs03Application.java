a56c53e844153261110608ea7eede42ce0015ff4
jarekjal <jarekjal@wp.pl>
2021-03-13 12:12:04 +0100

    init commit

---
+++ src/main/java/com/jarekjal/kurs03/Kurs03Application.java
@@ -0,0 +1,13 @@
+package com.jarekjal.kurs03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarekjal/kurs03/Kurs03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarekjal.kurs03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
