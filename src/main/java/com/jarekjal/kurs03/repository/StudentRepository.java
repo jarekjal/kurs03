221ebac56d3573916234ed727e1227106218889f
jarekjal <jarekjal@wp.pl>
2021-03-14 10:07:03 +0100

    po lekcji 21

--- src/main/java/com/jarekjal/kurs03/repository/StudentRepository.java
+++ src/main/java/com/jarekjal/kurs03/repository/StudentRepository.java
@@ -2,10 +2,12 @@ package com.jarekjal.kurs03.repository;
 
 import com.jarekjal.kurs03.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
 
 }
